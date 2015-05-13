f3b12e192b50ddf0b50b50186f7e7905f5a447fa
taivop <tpungas@ut.ee>
2015-05-13 22:31:36 +0300

    fix #2

--- src/article.py
+++ src/article.py
@@ -1,4 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from lxml import html
+import requests
 
 class Article:
     """A single article object."""
@@ -15,8 +17,11 @@ class Article:
         a = ["", self.title, str(self.date), ""] + chunks
         return "\n> ".join(a)
 
+
     @staticmethod
     def parse_date(date_string):
+        """Parse date string from article text into datetime object."""
+
         l = len(date_string)
         current = datetime.now()
 
@@ -25,18 +30,40 @@ class Article:
             return None
         # If only time
         elif l <= 5:
-            format = "%H:%M"
-            dt = datetime.strptime(date_string, format)
+            d_format = "%H:%M"
+            dt = datetime.strptime(date_string, d_format)
             dt = dt.replace(day=current.day, month=current.month, year=current.year)
         # If datetime without year
         elif l <= 11:
-            format = "%H:%M %d.%m"
-            dt = datetime.strptime(date_string, format)
+            d_format = "%H:%M %d.%m"
+            dt = datetime.strptime(date_string, d_format)
             dt = dt.replace(year=current.year)
         # If the whole shebang
         else:
-            format = "%H:%M %d.%m.%Y"
-            dt = datetime.strptime(date_string, format)
+            d_format = "%H:%M %d.%m.%Y"
+            dt = datetime.strptime(date_string, d_format)
         return dt
+
+    @staticmethod
+    def get_article(id):
+        """Download article with the given ID and return Article object built from it."""
+
+        # Build request URL
+        url = "http://wap.postimees.ee/index.php?wid=" + str(id)
+
+        # Pull page and parse HTML tree
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+
+        # Extract data from the fields
+        title = tree.xpath('//card/p/b/text()')
+        date = tree.xpath('//card/p[1]/text()')
+        text = tree.xpath('//card/p[2]/text()')
+
+        # Build Article object
+        if title and date and text:
+            return Article(title[0], date[0], text[0])
+        else:
+            return None
 
 
--- test/article_test.py
+++ test/article_test.py
@@ -10,5 +10,10 @@ class TestArticle(unittest.TestCase):
         self.assertEqual(Article.parse_date(""), None)
         self.assertEqual(str(Article.parse_date("13:29 08.09.2008")), "2008-09-08 13:29:00")
 
+    def test_download(self):
+        self.assertEqual(Article.get_article(318700).title, "Priit Rätsep kihutas Rahvuste krossil")
+        self.assertEqual(Article.get_article(31870), None)
+        self.assertEqual(str(Article.get_article(3).date), "2007-07-16 16:42:00")
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestArticle)
 unittest.TextTestRunner(verbosity=2).run(suite)
